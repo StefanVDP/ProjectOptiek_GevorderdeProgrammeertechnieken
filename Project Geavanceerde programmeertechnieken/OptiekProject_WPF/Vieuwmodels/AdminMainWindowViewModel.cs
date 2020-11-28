90f6c6c370a4078c5204239fdacd83ae479a105d
StefanVDP <61277081+StefanVDP@users.noreply.github.com>
2020-11-28 14:58:16 +0100

    tussentijdse push

---
+++ Project Geavanceerde programmeertechnieken/OptiekProject_WPF/Vieuwmodels/AdminMainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using OptiekProject_DAL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptiekProject_WPF.VieuwModels
+{
+    class AdminMainWindowViewModel : BasisViewModel
+    {
+        public override string this[string columnName]
+        {
+            get
+            {
+                return "";
+            }
+        }
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public override void Execute(object parameter)
+        {
+            switch (parameter.ToString())
+            {
+                case "Home":
+                    MainWindow mainwindow = new MainWindow();
+                    MainWindowViewModel MainViewModel = new MainWindowViewModel();
+                    mainwindow.DataContext = MainViewModel;
+                    mainwindow.Show();
+                    break;
+
+            }
+        }
+    }
+}
